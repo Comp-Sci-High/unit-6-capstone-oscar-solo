1fa5ca20dcab86e938142de24a68cea165276cf3
Oscar <oscar.altamirano25@compscihigh.org>
2025-05-13 20:45:54 +0000

    idljk

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ app.use((req, res, next) => {
 
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  cover: {
-    type: String,
-    default: ""
-  },
+  author: { type: String, reuired: true},
   pagesRead: { type: Number, default: 0 },
   totalPages: { type: Number, default: 1000 }
 });
@@ -42,28 +39,21 @@ app.patch("/update/:_id", async (req, res) => {
   const updated = await Book.findByIdAndUpdate(req.params._id, req.body, { new: true });
   res.json(updated);
 });
-
-
-async function prepopulateDb() {
-  try {
-    await Book.insertMany([
-      { title: "The Giver", totalPages: 180 },
-      { title: "Of Mice and Men", totalPages: 107 },
-      { title: "Fahrenheit 451", totalPages: 158 }
-    ]);
-
-    console.log('Books added successfully!');
-  } catch (err) {
-    console.error('Error prepopulating database:', err);
-  }
-}
+ app.post("/books/save", async (req, res) => {
+ const book = await new Book({
+ title: req.body.title,
+ author: req.body.author,
+ pagesRead: req.body.pagesRead,
+  totalPages: req.body.totalPages
+}).save()
+   res.json(book);
+ });
 
 //  Connect to MongoDB and Start Server
 async function startServer() {
   await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.lzdbf.mongodb.net/SE12?retryWrites=true&w=majority&appName=Cluster0");
 
-  // Uncomment ONCE to add sample books, then comment again
-  // await prepopulateDb();
+ 
 
   app.listen(3000, () => {
     console.log(`Server running.`);
